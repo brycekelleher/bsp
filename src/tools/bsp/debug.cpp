6f10f1ee16328e1f894fc53c2b48cdd5dda6819b
brycek <bryce_kelleher@hotmail.com>
2015-08-10 19:23:32 -0700

    tweak debug output for portals to make debuggin easier

--- src/tools/bsp/debug.cpp
+++ src/tools/bsp/debug.cpp
@@ -16,7 +16,6 @@ void DebugPrintfPolygon(FILE *fp, polygon_t *p)
 
 void DebugWritePolygon(FILE *fp, polygon_t *p)
 {
-	fprintf(fp, "color 1 0 0 1\n");
 	fprintf(fp, "polygon\n");
 	fprintf(fp, "%i\n", p->numvertices);
 	for(int i = 0; i < p->numvertices; i++)
@@ -28,7 +27,6 @@ void DebugWritePolygon(FILE *fp, polygon_t *p)
 
 void DebugWriteWireFillPolygon(FILE *fp, polygon_t *p)
 {
-	fprintf(fp, "color 1 0 0 1\n");
 	fprintf(fp, "polyline\n");
 	fprintf(fp, "%i\n", p->numvertices + 1);
 	for(int i = 0; i < p->numvertices; i++)
@@ -100,25 +98,40 @@ void DebugWritePortalFile(bsptree_t *tree)
 		if(!l->empty)
 			continue;
 		
-		for (portal_t *p = l->portals; p; p = p->leafnext)
+		// calculate a color for the portal polygon
 		{
-			//DebugWriteWireFillPolygon(fp, p->polygon);
-			vec3 center = l->box.Center();
-			
 			//fprintf(fp, "color 1 0 0 1\n");
 			float rgb[3];
 			HSVToRGB(rgb, (float)leafnum / tree->numleafs, 1.0f, 1.0f);
 			fprintf(fp, "color %f %f %f 1\n", rgb[0], rgb[1], rgb[2]);
+		}
+
+		for (portal_t *p = l->portals; p; p = p->leafnext)
+		{
+			vec3 center = l->box.Center();
 			
 			fprintf(fp, "polyline\n");
 			fprintf(fp, "%i\n", p->polygon->numvertices + 1);
 			
 			for(int i = 0; i < p->polygon->numvertices + 1; i++)
 			{
-				vec3 v =
-				center + (0.95f * (p->polygon->vertices[i % p->polygon->numvertices] - center));
-		
+				vec3 v = p->polygon->vertices[i % p->polygon->numvertices];
+
+				// scale the vertices so the indvidual portals are visible
+				{
+					vec3 boxsize =  l->box.Size();
+
+					vec3 scale;
+					scale[0] = (boxsize[0] - 8.0f) / boxsize[0];
+					scale[1] = (boxsize[1] - 8.0f) / boxsize[1];
+					scale[2] = (boxsize[2] - 8.0f) / boxsize[2];
+
+					v[0] = center[0] + (scale[0] * ( v[0] - center[0]));
+					v[1] = center[1] + (scale[1] * ( v[1] - center[1]));
+					v[2] = center[2] + (scale[2] * ( v[2] - center[2]));
+				}
 				
+				// write the vertex data
 				fprintf(fp, "%f %f %f\n",
 					v[0],
 					v[1],
@@ -128,6 +141,30 @@ void DebugWritePortalFile(bsptree_t *tree)
 	}
 	
 	fclose(fp);
-	// write portal data to file
+}
+
+static int leafnum = 0;
+void DebugWritePortalPolygon(bsptree_t *tree, polygon_t *p)
+{
+	static FILE *fp = NULL;
+
+	//if (leafnum != 1)
+	//	return;
+
+	if (!fp)
+		fp = FileOpenTextWrite("portal_src.gld");
+
+	// write the color
+	float rgb[3];
+	HSVToRGB(rgb, (float)leafnum / tree->numleafs, 1.0f, 1.0f);
+	fprintf(fp, "color %f %f %f 1\n", rgb[0], rgb[1], rgb[2]);
+
+	DebugWriteWireFillPolygon(fp, p);
+	fflush(fp);
+}
+
+void DebugEndLeafPolygons()
+{
+	leafnum++;
 }
 
